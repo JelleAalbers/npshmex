14a620f9f04a56706b7251dc3db236126ef10ddf
Jelle Aalbers <jelleaalbers@gmail.com>
2019-04-17 16:37:22 +0200

    Add description to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 import setuptools
 
-# Get requirements from requirements.txt, stripping the version tags
-with open('requirements.txt') as f:
-    requires = [x.strip().split('=')[0]
-                for x in f.readlines()]
+
+readme = open('README.md').read()
+history = open('HISTORY.md').read().replace('.. :changelog:', '')
+requirements = open('requirements.txt').read().splitlines()
 
 setuptools.setup(name='npshmex',
+                 author='Jelle Aalbers',
                  version='0.0.3',
                  description=('ProcessPoolExecutor that passes numpy'
                               'arrays through shared memory'),
                  py_modules=['multihist'],
+                 long_description=readme + '\n\n' + history,
                  install_requires=['SharedArray'],
                  setup_requires=['pytest-runner'],
-                 tests_require=requires + ['pytest'],
+                 tests_require=requirements + ['pytest'],
                  zip_safe=False)
