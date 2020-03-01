9317d888335b69728a97f03c400297745d0c4616
Jelle Aalbers <jelleaalbers@gmail.com>
2020-03-01 17:11:56 +0100

    Bump version: 0.1.2 → 0.2.0

--- npshmex.py
+++ npshmex.py
@@ -6,7 +6,7 @@ import string
 
 import SharedArray
 
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 
 _array_wrappers = dict()
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = open('requirements.txt').read().splitlines()
 
 setuptools.setup(name='npshmex',
                  author='Jelle Aalbers',
-                 version='0.1.2',
+                 version='0.2.0',
                  url='https://github.com/JelleAalbers/npshmex',
                  description=('ProcessPoolExecutor that passes numpy'
                               'arrays through shared memory'),
